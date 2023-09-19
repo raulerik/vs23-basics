64acce75cb754835e15bb7a91ed4e59fde2118de
Raul-Erik Laur <raul-erik.laur@voco.ee>
2023-09-19 12:02:48 +0300

    JS variables - declare, assign and use

---
+++ variables.js
@@ -0,0 +1,11 @@
+// variables
+// let
+// const
+// keyword variableName = value
+
+let programmingLanguage = 'Java Script';
+console.log(programmingLanguage)
+
+// CONST
+const group = 'VS23'
+console.log(group)
