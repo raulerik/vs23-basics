3cacdf9d2caca20492e84d13051c8c692806c325
Raul-Erik Laur <raul-erik.laur@voco.ee>
2023-09-19 13:53:30 +0300

    added datatypes

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+// data types
+// String
+let firstName = 'Raul-Erik'
+let lastName = 'Laur'
+console.log(firstName + ' ' + lastName)
+
+// Numbers
+let goodMark = 5
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+// Boolean
+let isToday = true
+console.log(typeof isToday)
+
+// type conversions
+// number - String
+console.log(typeof String(555))
